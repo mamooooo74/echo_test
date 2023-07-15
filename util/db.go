1d2845a3db91b805700bc205f48634b8f528bdcd
mamoru <mamooo.74@icloud.com>
2023-07-15 12:20:33 +0900

    envを設定

--- util/db.go
+++ util/db.go
@@ -3,13 +3,23 @@ package util
 import (
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func NewDB() *gorm.DB {
-	url := "root@tcp(127.0.0.1:3306)/go?charset=utf8mb4&parseTime=True&loc=Local"
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatalln(err)
+	}
+	url := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"))
 	db, err := gorm.Open(mysql.Open(url), &gorm.Config{})
 	if err != nil {
 		log.Fatalln(err)
