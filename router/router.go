272574bd296ed5a85fcf1eb7d239f6e502364bfd
mamoru <mamooo.74@icloud.com>
2023-07-16 10:00:21 +0900

    cors対策

--- router/router.go
+++ router/router.go
@@ -1,13 +1,20 @@
 package router
 
 import (
+	"net/http"
 	"test_api/api/controller"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func NewRouter() *echo.Echo {
 	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{http.MethodGet},
+	}))
+
 	t := e.Group("/tasks")
 	tc := controller.NewTaskController()
 	t.GET("/", tc.GetAllTask)
